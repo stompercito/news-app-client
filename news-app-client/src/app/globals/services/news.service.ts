af8a603356ed3d5e1a6881d541947b9a9cb5c012
stompercito <51408323+stompercito@users.noreply.github.com>
2020-11-02 17:29:59 -0600

    Services updated
    Now the services are working whit the back end insted of the news api

--- news-app-client/src/app/globals/services/news.service.ts
+++ news-app-client/src/app/globals/services/news.service.ts
@@ -8,38 +8,31 @@ export class NewsService {
 
   constructor(private httpClient:HttpClient) { }
 
+  apiUrl:String="https://newsapi.org/v2/";
+  apiUrl2:String="http://localhost:3000/";
+  apiKey:String="1e30c02c90b84a46a828833a44569f18";
+  apiKey2:String="0baf3e670623453293527fd712ced36a";
+
   getSources():Promise<any>  {
-    const apiUrl="https://newsapi.org/v2/";
-    const apiKey="1e30c02c90b84a46a828833a44569f18";
-    const apiKey2="0baf3e670623453293527fd712ced36a";
-    const url = `${apiUrl}/sources?apiKey=${apiKey2}`;
+    const url = `${this.apiUrl2}fuentes`;
     return this.httpClient.get(url).toPromise();
     //return this.http.get('https://jsonplaceholder.typicode.com/posts').toPromise();
   }
 
   getNewsByCountry(country:String):Promise<any>  {
-    const apiUrl="https://newsapi.org/v2/";
-    const apiKey="1e30c02c90b84a46a828833a44569f18";
-    const apiKey2="0baf3e670623453293527fd712ced36a";
-    const url = `${apiUrl}/top-headlines?country=${country}&apiKey=${apiKey2}`;
+    const url = `${this.apiUrl2}top-headlines?country=${country}`;
     return this.httpClient.get(url).toPromise();
     //return this.http.get('https://jsonplaceholder.typicode.com/posts').toPromise();
   }
 
   getNewsByQuery(query:String):Promise<any>  {
-    const apiUrl="https://newsapi.org/v2/";
-    const apiKey="1e30c02c90b84a46a828833a44569f18";
-    const apiKey2="0baf3e670623453293527fd712ced36a";
-    const url = `${apiUrl}/everything?q=${query}&apiKey=${apiKey2}`;
+    const url = `${this.apiUrl2}noticias?q=${query}`;
     return this.httpClient.get(url).toPromise();
     //return this.http.get('https://jsonplaceholder.typicode.com/posts').toPromise();
   }
 
   getNewsByQueryAndSource(query:String, source:String):Promise<any>  {
-    const apiUrl="https://newsapi.org/v2/";
-    const apiKey="1e30c02c90b84a46a828833a44569f18";
-    const apiKey2="0baf3e670623453293527fd712ced36a";
-    const url = `${apiUrl}/everything?sources=${source}&q=${query}&apiKey=${apiKey2}`;
+    const url = `${this.apiUrl2}noticias?q=${query}&source=${source}`;
     return this.httpClient.get(url).toPromise();
     //return this.http.get('https://jsonplaceholder.typicode.com/posts').toPromise();
   }
